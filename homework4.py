ea64b6afe3f6d839685882a732a42e83fe2f911e
Skip-GT <yaroslav.vasilev.2015@gmail.com>
2024-06-28 17:58:40 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+my_string = input(len('Практика'))
+print(my_string)
+name = 'Приветствую, это строка'
+print(name.upper())
+print(name.lower())
+print(name.replace(' ',''))
+print(name[0])
+print(name[-1:])
